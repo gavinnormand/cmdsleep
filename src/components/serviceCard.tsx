52e630b6c5ae12f374bba4fc083972aa640a3925
gavinnormand <normandgavin@gmail.com>
2025-07-28 11:22:19 -0400

    services page done

---
+++ src/components/serviceCard.tsx
@@ -0,0 +1,99 @@
+import { ChevronRight } from "lucide-react";
+import { useState } from "react";
+
+interface faq {
+  question: string;
+  answer: string;
+}
+
+interface ServiceCardProps {
+  name: string;
+  description: string;
+  instructions: string[];
+  faqs: faq[];
+  infoLink: string;
+}
+
+const ServiceCard: React.FC<ServiceCardProps> = ({
+  name,
+  description,
+  instructions,
+  faqs,
+  infoLink,
+}) => {
+  const [showInstructions, setShowInstructions] = useState(false);
+
+  const toggleInstructions = () => {
+    setShowInstructions(!showInstructions);
+  };
+
+  const [showFAQs, setShowFAQs] = useState(false);
+  const toggleFAQs = () => {
+    setShowFAQs(!showFAQs);
+  };
+
+  return (
+    <div className="flex w-full flex-col justify-between justify-self-center rounded-xl border-2 border-blue-800 bg-sky-50 p-6 text-center shadow-lg">
+      <div className="flex flex-col gap-y-3">
+        <p className="text-center text-4xl font-semibold text-blue-800">
+          {name}
+        </p>
+        <p className="text-lg">{description}</p>
+      </div>
+      <button onClick={toggleInstructions} className="w-fit">
+        <div className="inline-flex items-center">
+          <ChevronRight
+            className={`inline transition-transform duration-300 ${
+              showInstructions ? "rotate-90" : "rotate-0"
+            }`}
+          />
+          <h1 className="text-lg font-semibold">Instructions</h1>
+        </div>
+      </button>
+      {showInstructions && (
+        <div className="ml-2 flex flex-col items-start">
+          {instructions.map((instruction, index) => (
+            <p key={index} className="text-lg">
+              <span className="font-semibold">{index + 1}.</span> {instruction}
+            </p>
+          ))}
+        </div>
+      )}
+      <button onClick={toggleFAQs} className="w-fit">
+        <div className="mt-2 inline-flex items-center">
+          <ChevronRight
+            className={`inline transition-transform duration-300 ${
+              showFAQs ? "rotate-90" : "rotate-0"
+            }`}
+          />
+          <h1 className="text-lg font-semibold">Frequently Asked Questions</h1>
+        </div>
+      </button>
+      {showFAQs && (
+        <div className="ml-2 flex flex-col items-start gap-y-2 text-left">
+          {faqs.map((faq) => (
+            <div className="flex flex-col items-start text-lg">
+              <p>
+                <span className="font-semibold">{"Q: "}</span> {faq.question}
+              </p>
+              <p>
+                <span className="font-semibold">{"A: "}</span> {faq.answer}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+      <a
+        href={infoLink}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="mx-auto mt-8 w-1/2 rounded-md border-2 border-blue-600 p-3 text-center align-baseline font-semibold text-blue-800 transition-all hover:bg-blue-600 hover:text-white active:bg-blue-600 active:text-white"
+      >
+        {" "}
+        More Info
+      </a>
+    </div>
+  );
+};
+
+export default ServiceCard;
--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,5 +1,58 @@
+import PageTitle from "../components/pageTitle";
+import ServiceCard from "../components/serviceCard";
+
 function Services() {
-  return <div>Services Page</div>;
+  return (
+    <div>
+      <PageTitle text={"Services"} />
+      <div className="flex flex-row flex-wrap justify-center gap-8 p-8 md:gap-12 md:p-12">
+        <ServiceCard
+          name={"Home Sleep Study"}
+          description={
+            "test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description"
+          }
+          instructions={["test1", "test2", "test3"]}
+          faqs={[
+            {
+              question: "What is a Home Sleep Study?",
+              answer:
+                "A Home Sleep Study is a test that allows you to monitor your sleep patterns in the comfort of your own home.",
+            },
+            {
+              question: "How do I prepare for a Home Sleep Study?",
+              answer:
+                "Preparation involves following specific guidelines provided by your healthcare provider, such as avoiding caffeine and alcohol before the test.",
+            },
+          ]}
+          infoLink={
+            "https://www.sleepfoundation.org/sleep-apnea/home-sleep-tests"
+          }
+        />
+        <ServiceCard
+          name={"Polysomnogram"}
+          description={
+            "test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description test description"
+          }
+          instructions={["test1", "test2", "test3"]}
+          faqs={[
+            {
+              question: "What is a Polysomnogram?",
+              answer:
+                "A Polysomnogram is a comprehensive sleep study that records brain waves, oxygen levels, heart rate, and breathing, as well as eye and leg movements.",
+            },
+            {
+              question: "How long does a Polysomnogram take?",
+              answer:
+                "Typically, a Polysomnogram lasts for one night, but it may vary based on individual needs.",
+            },
+          ]}
+          infoLink={
+            "https://www.sleepfoundation.org/sleep-studies/polysomnography"
+          }
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Services;
