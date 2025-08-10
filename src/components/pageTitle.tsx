3f3ddc61450cbdd7288939573a65ad2f6c89e963
gavinnormand <normandgavin@gmail.com>
2025-08-09 23:28:01 -0400

    Change page title padding

--- src/components/pageTitle.tsx
+++ src/components/pageTitle.tsx
@@ -5,7 +5,7 @@ interface PageTitleProps {
 const PageTitle: React.FC<PageTitleProps> = ({ text }) => {
   return (
     <div className="bg-secondary-light flex h-16 w-full items-center py-24 text-center">
-      <h1 className="text-primary mx-auto text-6xl font-bold">{text}</h1>
+      <h1 className="text-primary mx-auto text-6xl font-bold px-8">{text}</h1>
     </div>
   );
 };
