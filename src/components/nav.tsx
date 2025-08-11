d5fc3429d2d52de96a0c30fe781f4d425b25402c
gavinnormand <normandgavin@gmail.com>
2025-08-11 15:52:13 -0400

    Add home to tabs

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -8,13 +8,19 @@ function Nav() {
 
   return (
     <nav className="sticky top-0 z-50 bg-white">
-      <p className="text-center">In business since 2009 ~ Licensed by the State of New Jersey and Accreditate with AASM</p>
+      <p className="text-center">
+        In business since 2009 ~ Licensed by the State of New Jersey and
+        Accreditate with AASM
+      </p>
       {/* Desktop nav bar */}
       <div className="hidden items-center justify-between px-8 py-4 xl:flex">
         <Link to="/">
           <img src="/cmdsleepLogo.png" className="h-12"></img>
         </Link>
         <div className="inline-flex space-x-4 text-lg">
+          <Link to="/" className="hover:underline">
+            Home
+          </Link>
           <Link to="/patient-portal" className="hover:underline">
             Patient Portal
           </Link>
@@ -45,17 +51,17 @@ function Nav() {
       {/* Mobile Layout */}
       <div className="grid grid-cols-[1fr_20fr_1fr] justify-between px-6 py-4 xl:hidden">
         <div className="flex items-center">
-        <button
-          className="text-icon hover:text-icon-hover cursor-pointer"
-          aria-label="Toggle menu"
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
-        >
-          {isMenuOpen ? (
-            <X className="h-8 w-8" />
-          ) : (
-            <MenuIcon className="h-8 w-8" />
-          )}
-        </button>
+          <button
+            className="text-icon hover:text-icon-hover cursor-pointer"
+            aria-label="Toggle menu"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            {isMenuOpen ? (
+              <X className="h-8 w-8" />
+            ) : (
+              <MenuIcon className="h-8 w-8" />
+            )}
+          </button>
         </div>
         <Link to="/" className="flex items-center justify-center">
           <img src="/cmdsleepLogo.png" className="h-12 object-contain"></img>
